066a0a7156e3e65eeec4e2ad0661b18e37c7cc0b
Salvatore <salvatore.foderaro@gmail.com>
2017-11-24 11:27:04 +0100

    Primo commit

---
+++ demoProgetto.py
@@ -0,0 +1,101 @@
+# coding=utf-8
+from module.binaryTree import BinaryTree
+from module.binaryTree import BinaryNode
+from module.dictBinaryTree import DictBinaryTree
+from sorgenteProgetto import *
+import random
+import time
+
+def dictLazyBench(n):
+    """La funzione mi permette di calcolare il tempo di esecuzione medio delle tre funzioni implementate nel nuovo dizionario"""
+    print "\nTempo impiegato con ", n," valori"
+    alb3 = DictLazy()
+    for i in range (1,n):
+        alb3.lazyInsert(random.randint(0,n*2),random.randint(0,n*2))
+
+    start = time.time()
+    for k in range (1,n):
+        alb3.boolDeleteLazy(random.randint(0,n*2))
+    end = time.time()
+    elapsed = (end - start)
+    print "Tempo boolDeleteLazy:", elapsed,"secondi"
+
+    start = time.time()
+    for j in range (1,n):
+        alb3.lazySearch(random.randint(0,n*2))
+    end = time.time()
+    elapsed = (end - start)
+    print "Tempo lazySearch:", elapsed,"secondi"
+
+    start = time.time()
+    for m in range (1,n):
+        alb3.insertLazyControl(random.randint(0,n*2), random.randint(0,n*2))
+    end = time.time()
+    elapsed = (end - start)
+    print "Tempo insertLazyControl:", elapsed, "secondi"
+
+
+def dictLazyGUI():
+    """La funzione mi permette di mostrare a video, a mo di interfaccia grafica, una Demo di esecuzione delle tre funzioni implementate"""
+    print "\n                <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>          \n"
+
+    print "Creo un Albero con le proprietà di Lazy Search ---------------> alb2 = DictLazy()  "
+    print "Sfrutto un ciclo for per inserire 10 elementi nell'albero"
+    alb2 = DictLazy()
+    for i in range(1, 10):
+        alb2.lazyInsert(i, i)
+    print "\n                <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< bool delete (key k) >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>          \n"
+    print "=================== Funzione da implementare ==================="
+    print "\nSe il nodo con chiave k e' presente nell albero e non e' segnato come eliminato, segna il nodo come eliminato e ritorna True.\nSe il nodo con chiave k non e' presente nell albero, o e' presente ma e' stato gia' precedentemente segnato come eliminato, ritorna False."
+    print "\n=================== Demo implementazione ==================="
+    node = alb2.searchNode(5)
+    print "\nAssocio alla variabile node il nodo dell'albero con chiave 5 ---------------> node = alb2.searchNode(5)"
+    print "Il nodo con chiave 5 e' segnato come eliminato? ------- alb2.isDeletedOr(node) --------> ", alb2.isDeletedOr(node)
+    print "Cosa restituisce la funzione boolDeleteLazy(5)? ------- alb2.boolDeleteLazy(5) --------> ", alb2.boolDeleteLazy(5)
+    print "Il nodo con chiave 5 e' segnato come eliminato? ------- alb2.isDeletedOr(node) --------> ", alb2.isDeletedOr(node)
+    print "\nIl nodo con chiave 5 e' segnato come eliminato? ------- alb2.isDeletedOr(node) --------> ", alb2.isDeletedOr(node)
+    print "Cosa restituisce la funzione boolDeleteLazy(5)? ------- alb2.boolDeleteLazy(5) --------> ", alb2.boolDeleteLazy(5)
+    print "\nCosa restituisce la funzione boolDeleteLazy(50)? [Elemento non apparteente all'array] ------- alb2.boolDeleteLazy(50) -------->  ", alb2.boolDeleteLazy(50)
+    print "\n                <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< search (key k) >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>          \n"
+    print "=================== Funzione da implementare ==================="
+    print "\nRitorna il nodo con chiave k se questo è presente nell’albero e non è segnato come eliminato."
+    print "\n=================== Demo implementazione ==================="
+    node1 = alb2.searchNode(9)
+    print "\nAssocio alla variabile node1 il nodo dell'albero con chiave 9 ---------------> node1 = alb2.searchNode(9)"
+    print "Il nodo con chiave 9 e' segnato come eliminato? ------- alb2.isDeletedOr(node1) --------> ", alb2.isDeletedOr(node1)
+    print "Cosa restituisce la funzione lazySearch(9)? ------- alb2.lazySearch(9) --------> ", alb2.lazySearch(9)
+    print "\nImposto il nodo come eliminato ---------------> alb2.setDeleted(node1)"
+    alb2.setDeleted(node1)
+    print "\nIl nodo con chiave 9 è segnato come eliminato? ------- alb2.isDeletedOr(node1) --------> ", alb2.isDeletedOr(node1)
+    print "Cosa restituisce la funzione lazySearch(9)? ------- alb2.lazySearch(9) --------> ", alb2.lazySearch(9)
+    print "\nCosa restituisce la funzione lazySearch(100)? [Elemento non appartenente all'array] ------- alb2.lazySearch(100) -------->", alb2.lazySearch(100)
+    print "\n                <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Insert (key k, value v) >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>          \n"
+    print "=================== Funzione da implementare ==================="
+    print "\nInserisce una nuova coppia (k,v) nell’albero binario. Se è possibile inserire il nodo con chiave k nella posizione di un nodo segnato come eliminato, sostituisce il vecchio nodo co  il nuovo."
+    print "\n=================== Demo implementazione ==================="
+    node3 = alb2.searchNode(3)
+    print "\nAssocio alla variabile node3 il nodo dell'albero con chiave 9 ---------------> node3 = alb2.searchNode(3)"
+    print "Imposto il nodo con chiave 3 come eliminato ---------------> alb2.setDeleted(node3)"
+    alb2.setDeleted(node3)
+    print "Inserisco un nuovo elemento con chiave 3 e valore 200 ---------------> alb2.insertLazyControl(3, 200)"
+    alb2.insertLazyControl(3, 200)
+    print "Qual'è il valore del nuovo nodo, nella posizione del precedente? ------- alb2.value(node3) -------->", alb2.value(node3)
+    print "\nAlbero con valore sostituito:"
+    alb2.tree.stampa()
+    node4 = alb2.searchNode(4)
+    print "\nAssocio alla variabile node4 il nodo dell'albero con chiave 4 ---------------> node4 = alb2.searchNode(4)"
+    print "Il nodo con chiave 4 è segnato come eliminato? ------- alb2.isDeletedOr(node4) --------> ", alb2.isDeletedOr(node4)
+    print "Inserisco un nuovo elemento con chiave 4 e valore 157 ---------------> alb2.insertLazyControl(4, 157)"
+    alb2.insertLazyControl(4, 157)
+    print "\nAlbero con nuovo elemento inserito:"
+    alb2.tree.stampa()
+    print "\n                <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Benchmark Tempo >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>          \n"
+
+if __name__ == "__main__":
+
+    dictLazyGUI()
+    dictLazyBench(100)
+    dictLazyBench(200)
+    dictLazyBench(300)
+
+
---
+++ sorgenteProgetto.py
@@ -0,0 +1,125 @@
+# coding: utf-8
+"""
+    File name: sorgenteProgetto.py
+    Author: Salvatore Foderaro
+    Date created: 16/11/2017
+    Modified By: Salvatore Foderaro
+    Date last modified: [Insert Date]
+    Python Version: 2.7.14
+
+    Questo modulo implementa le tre funzioni richieste dal primo problema della prima prova in Itenere AA 2017/2018:
+
+    - bool delete(key k) è stata impelemntata con la funzione boolDeleteLazy
+    - insert(key k, value v) è stata implementata con la funzione insertLazyControl
+    - search(chiave k) è stata implementata con la funzione lazySearch
+"""
+
+from module.binaryTree import BinaryTree
+from module.binaryTree import BinaryNode
+from module.dictBinaryTree import DictBinaryTree
+import random
+import time
+
+class DictLazy(DictBinaryTree):
+    def __init__(self):
+        self.tree=BinaryTree() # Le informazioni del nodo sono ora una tripla [key, value, lazyDelete]
+
+    def deletionValue(self,node): 
+        # Restituisce il valore di "eliminazione" del nodo (0 non eliminato, 1 eliminato)
+        if node==None:
+            return None #aiuta a calcolare il balance factor
+        return node.info[2]
+
+    def setDeleted(self,node):
+        # Imposta il valore di eliminazione del nodo ad 1 (eliminato)
+        if node == None:
+            return None
+        node.info[2] = 1
+        
+    def setUnDeleted(self,node):
+        # Imposta il valore di eliminazione del nodo a 0 (Non eliminato)
+        if node == None:
+            return None
+        node.info[2] = 0
+
+    def isDeletedOr(self,node):
+        # Controlla il valore di eliminazione del nodo. Ritorna True se il nodo è eliminato, mentre False se il nodo non è segnato come eliminato
+        if node == None:
+            return None
+        elif self.deletionValue(node) == 0:
+            return False
+        else:
+            return True
+
+    def setValue(self,node,value):
+        # Permette di impostare un valore preciso per un nodo
+        if node == None:
+            return None
+        node.info[1] = value
+
+    def setKey(self,node,newKey):
+        # Permette di impostare una chiave specifica per un nodo
+        if node == None:
+            return None
+        node.info[0] = newKey
+    
+    def lazySearch(self,key):
+        """ search(chiave k) : Ritorna il nodo con chiave k se questo è
+            presente nell’albero e non è segnato come eliminato. """
+        
+        node = self.searchNode(key)          # Cerco il nodo con la chiave che sto cercando
+        if self.isDeletedOr(node) == False:  # Controllo che il nodo non sia impostato come eliminato
+           return self.searchNode(key)       # Restituisco il nodo
+        else:                                # Altrimenti non restituisco nulla
+           return None
+    
+    def boolDeleteLazy(self,key):
+        """ bool delete(key k) : Se il nodo con chiave k è presente nell’albero
+            e non è segnato come “eliminato”, segna il nodo come eliminato e
+            ritorna True. Se il nodo con chiave k non è presente nell’albero, o
+            è presente ma è stato già precedentemente segnato come eliminato, ritorna False"""
+        
+        nodo = self.lazySearch(key)  # Trovo il nodo con valore key all'interno dell'albero
+        if nodo != None:             # Controllo se il nodo con chiave k è presente nell'albero e se è segnato come eliminato o meno
+           self.setDeleted(nodo)     # Segno il nodo come eliminato
+           return True               # Restituisco True
+        return False                 # Altrimenti restituisco False
+
+    def insertLazyControl(self,key,value):
+        """ Insert(key k, value v) : Inserisce una nuova coppia (k,v) nell’albero
+            binario. Se è possibile inserire il nodo con chiave k nella posizione
+            di un nodo segnato come eliminato, sostituisce il vecchio nodo con
+            il nuovo. """
+        maybeNode = self.searchNode(key)             # Cerco, se presente, un nodo con la stessa chiave key che sto cercando di inserire
+        if self.isDeletedOr(maybeNode) == True:      # Controllo se già presente e se è impostato come eliminato
+            # self.setKey(maybeNode, key)              # Nel caso in cui la condizione sia verificata, i
+            self.setValue(maybeNode,value)           # Nel caso in cui la condizione sia verificata, copio il valore del vecchio nodo nel nuovo
+            self.setUnDeleted(maybeNode)             # e lo imposto come non eliminato
+        else:
+            self.lazyInsert(key,value)               # Altrimenti procedo al normale inserimento
+            
+
+    def lazyInsert(self, key, value, lazyDelete=0):
+        """Insert normale come visto a lezione, con l'unica differenza che oltre a key e value, è presente anche il nuovo valore lazyDelete.
+           Se non indicato diversamente, il valore viene impostato a 0, cioè come non eliminato secondo il dizionario che ho impostato """
+        pair = [key, value, lazyDelete]
+        newt = BinaryTree(BinaryNode(pair))
+        if self.tree.root == None:
+            self.tree.root = newt.root
+        else:
+            curr = self.tree.root 
+            pred = None             
+            while curr != None:
+              pred = curr
+              if key <= self.key(curr): 
+                  curr = curr.leftSon
+              else:
+                  curr = curr.rightSon
+            if key <= self.key(pred):
+                self.tree.insertAsLeftSubTree(pred, newt)
+            else:
+                self.tree.insertAsRightSubTree(pred, newt)
+
+        
+if __name__ == "__main__":
+    print
