e1ca43617856ca6219ca5a160549e2d7236a094f
Salvatore <salvatore.foderaro@gmail.com>
2018-01-09 19:33:02 +0100

    Commit!

--- 2° Progetto in Itinere/Graph.py
+++ 2° Progetto in Itinere/Graph.py
@@ -273,47 +273,56 @@ class GraphBase(Graph, metaclass=ABCMeta):
         ...
 
     def controlloFunzione(self):
-        nodi = []
-        nodeMax = [0, 0]
-        for nodo in (self.getNodes()):
-            if self.getAdjModified(nodo.id) != 0:
-                nodi.append(nodo.id)
+        """
+        Questa funzione, dato un grafo, restituisce una lista contenente la lista dei nodi massimi ed il numero di volte
+        che risultano medi all'interno del grafo.
 
-        while (len(nodi)>0):
-            percorso = self.mediumNode(random.choice(nodi))
-            if percorso[1] != 0:
-                nodoMassimo = self.backToFather(percorso[1])
-                if(nodoMassimo != 0 and nodoMassimo[1] > nodeMax[1]):
-                    nodeMax[0] = nodoMassimo[0]
+        :return: restituisco il nodo che risulta massimo nel grafo
+        """
+
+        nodi = []  # Lista dei nodi appartenenti al grafo, con almeno un elemento adiacente
+        nodeMax = [0, 0]  # Informazioni sul nodo che risulta medio il maggior numero di volte
+        for nodo in (self.getNodes()):  # Considero ogni nodo
+            if (self.getAdjModified(nodo.id) == 0):
+            if (self.getAdjModified(nodo.id) != 0):  # Se il nodo ha almeno un nodo adiacente,
+                nodi.append(nodo.id)  # lo aggiungo alla lista dei nodi da visitare
+
+        while (len(nodi) > 0):  # Fin quando ho nodi da di visitare
+            percorso = self.mediumNode(random.choice(nodi))  # Ottengo il percorso più lungo nel grafo
+            if percorso[1] != 0:  # Se esiste un percorso,
+                nodoMassimo = self.backToFather(percorso[3])  # calcolo il valore del(dei) nodo massimo(massimi)
+                if (nodoMassimo != 0 and nodoMassimo[1] > nodeMax[
+                    1]):  # Se il nuovo nodo è medio per un numero superiore di volte all'attuale massimo,
+                    nodeMax[0] = nodoMassimo[0]  # imposto i suoi valori come nuovo massimo
                     nodeMax[1] = nodoMassimo[1]
-            nodi = list(set(nodi) - set(percorso[2]))
+            nodi = list(set(nodi) - set(
+                percorso[2]))  # Rimuovo dalla lista dei nodi quelli appartenenti al sottografo considerato
 
-        return nodeMax
+        return nodeMax  # Restituisco il nodo massimo e le volte che risulta massimo nel grafo
 
-    def backToFather(self, rootID):
-        nodeList = [0, 0]
-        percorso = []  # La lista dei nodi appartenenti al percorso più lungo
-        while (rootID.father != None and rootID != int):  # Fin quando il nodo che sto considerando ha un padre,
-            percorso.append(rootID.info)  # aggiungo il padre alla lista dei nodi appartenenti al percorso,
-            rootID = rootID.father  # imposto il padre come nuovo nodo
+    def backToFather(self, percorso):
+        """
+        Questa funzione, dato un grafo ed il percorso più lungo all'interno di un suo sottografo, restituisce una lista contenente l'Id
+        del nodo (o dei nodi) ed il numero di volte che risultano medi.
+
+        :param percorso: lista dei nodi appartenenti al percorso
+        :return: lista contenente le informazioni sul nodo massimo
+        """
+        nodeList = [0, 0]  # Informazioni riguardo al nodo massimo
 
         if len(percorso) < 3:  # Se ho meno di tre elementi nel percorso, nessun nodo risulta medio
             return 0
 
         if (len(
                 percorso) % 2) == 0:  # Se il numero di elementi è pari, devo controllare quale dei due elementi ha il maggior numero di figli
-
             # Ottengo i due elementi medi nella lista
             primoElemento = percorso[int(len(percorso) / 2)]
             secondoElemento = percorso[int((len(percorso) / 2) + 1)]
 
-            # Elimino l'arco che collega i due nodi, in modo da poter eseguire due visite separate
-            self.deleteEdge(primoElemento, secondoElemento)
-            self.deleteEdge(secondoElemento, primoElemento)
+            first = len(percorso[:percorso.index(primoElemento)])  # Numero di nodi figli del primo elemento
+            second = len(percorso[percorso.index(secondoElemento):])  # Numero di nodi figli del secondo elemento
 
-            first = self.calculateSubNode(primoElemento)  # Numero di nodi figli del primo elemento
-            second = self.calculateSubNode(secondoElemento)  # Numero di nodi figli del secondo elemento
-            # Devo aggiungere il return della lunghezza del percorso
+            # Confronto il numero di elementi appartenenti ai sottoalberi ottenuti dai due elementi
             if first < second:
                 nodeList = [secondoElemento, second]
             elif second > first:
@@ -321,45 +330,26 @@ class GraphBase(Graph, metaclass=ABCMeta):
             else:
                 nodeList = [[primoElemento, secondoElemento], first]
         else:
-                nodeList = [percorso[int(len(percorso)/2)], int(len(percorso)/2)]
+            # Se il numero di elementi nel percorso è dispari, prendo quello che sta a metà
+            nodeList = [percorso[int(len(percorso) / 2)], int(len(percorso) / 2)]
+
         return nodeList
-
-    def calculateSubNode(self, rootId):
-        """
-        Execute a generic search in the graph starting from the specified node.
-        :param rootId: the root node ID (integer).
-        :return: the generic exploration tree.
-        """
-
-        counter = 0  # Contatore degli elementi figli del nodo
-
-        # Utilizzo l'algoritmo per la visita generica visto a lezione
-        if rootId not in self.nodes:
-            return None
-
-        treeNode = TreeNode(rootId)
-        vertexSet = {treeNode}
-        markedNodes = {rootId}
-
-        while len(vertexSet) > 0:
-            treeNode = vertexSet.pop()
-            adjacentNodes = self.getAdj(treeNode.info)
-            for nodeIndex in adjacentNodes:
-                if nodeIndex not in markedNodes:
-                    counter = counter + 1  # Incremento il contatore
-                    newTreeNode = TreeNode(nodeIndex)
-                    vertexSet.add(newTreeNode)
-                    markedNodes.add(nodeIndex)
-        return counter
 
     def mediumNode(self, rootId):
         """
-        Execute a generic search in the graph starting from the specified node.
-        :param rootId: the root node ID (integer).
-        :return: the generic exploration tree.
+        Questa funzione, dato un grafo e l'id di un nodo, esegue una visita generica. Per ogni foglia, chiama la funzione
+        leafDistance, confrontando i valori ottenuti con quelli della lista massima.
+
+        :param Id della radice da cui far partire la visita
+        :return: lista contenente le informazioni sul nodo massimo e sul percorso
         """
 
-        max = [0, 0, 0]  # Inizializzo a 0 le informazioni riguardo al nodo massimo
+        max = [0, 0, 0, 0]  # Inizializzo a 0 le informazioni riguardo al nodo massimo
+
+        # max[0] = lunghezza del percorso
+        # max[1] = foglia più profonda
+        # max[2] = lista dei nodi visitati
+        # max[3] = nodi appartenenti al percorso più lungo
 
         # Utilizzando l'algoritmo per la visita generica visto a lezione, scansiono l'albero
         if rootId not in self.nodes:
@@ -367,7 +357,7 @@ class GraphBase(Graph, metaclass=ABCMeta):
 
         treeNode = TreeNode(rootId)
         vertexSet = {treeNode}
-        markedNodes = {rootId}  # Nodi visitati
+        markedNodes = [rootId]  # Nodi visitati
 
         while len(vertexSet) > 0:
             treeNode = vertexSet.pop()
@@ -381,6 +371,7 @@ class GraphBase(Graph, metaclass=ABCMeta):
                     0]:  # Se il percorso è più lungo dell'attuale massimo, imposto i valori della foglia
                     max[0] = lunghezzaPercorso[0]
                     max[1] = lunghezzaPercorso[1]
+                    max[3] = lunghezzaPercorso[2]
 
             for nodeIndex in adjacentNodes:
                 if nodeIndex not in markedNodes:
@@ -388,19 +379,20 @@ class GraphBase(Graph, metaclass=ABCMeta):
                     newTreeNode.father = treeNode
                     treeNode.sons.append(newTreeNode)
                     vertexSet.add(newTreeNode)
-                    markedNodes.add(nodeIndex)
+                    markedNodes.append(nodeIndex)
 
         max[2] = markedNodes
         return max
 
     def leafDistance(self, rootId):
         """
-        Execute a generic search in the graph starting from the specified node.
+        Questa funzione, dato un grafo e l'id di una foglia, restituisce il percoso più lungo e la sua lunghezza.
+
         :param rootId: the root node ID (integer).
         :return: the generic exploration tree.
         """
 
-        lunghezzaPercorso = [0, 0]
+        lunghezzaPercorso = [0, 0, 0]
         # lunghezzaPercorso[0] = distanza del nodo dalla radice
         # lunghezzaPercorso[1] = nodo
 
@@ -410,7 +402,7 @@ class GraphBase(Graph, metaclass=ABCMeta):
 
         treeNode = TreeNode(rootId)
         vertexSet = {treeNode}
-        markedNodes = {rootId}
+        markedNodes = [rootId]
 
         while len(vertexSet) > 0:
             treeNode = vertexSet.pop()
@@ -433,7 +425,8 @@ class GraphBase(Graph, metaclass=ABCMeta):
                     newTreeNode.distanza = treeNode.distanza + 1  # Incremento la distanza del nodo
                     treeNode.sons.append(newTreeNode)
                     vertexSet.add(newTreeNode)
-                    markedNodes.add(nodeIndex)
+                    markedNodes.append(nodeIndex)
+        lunghezzaPercorso[2] = markedNodes
 
         return lunghezzaPercorso  # Restituisco la lista con i valori
 
--- 2° Progetto in Itinere/demoAlgoritmo.py
+++ 2° Progetto in Itinere/demoAlgoritmo.py
@@ -4,8 +4,8 @@ from Graph_IncidenceList import GraphIncidenceList
 import time
 import random
 
-
 def generateRandomGraph(numNodes, numEdges, strutturaDati):
+    print("\n", numNodes, "nodi e", numEdges, "archi")
 
     if numEdges > numNodes:
         print("Errore, il numero di nodi non può essere minore del numero degli archi")
@@ -22,19 +22,42 @@ def generateRandomGraph(numNodes, numEdges, strutturaDati):
     for j in range (1, numEdges):
         a = random.choice(nodes)
         b = random.choice(nodes)
-        if (a != b and b not in heads):
-            heads.append(b)
-            graph.insertEdge(a,b)
-            graph.insertEdge(b,a)
+        if (a != b):
+            if (b not in heads):
+                heads.append(b)
+                graph.insertEdge(a,b)
+                graph.insertEdge(b,a)
 
-    print("Funzione!")
     start = time.time()
-    print(graph.controlloFunzione())
+    graph.controlloFunzione()
     end = time.time()
     print(end - start)
 
 if __name__ == "__main__":
-    for k in range(1, 3):
-         generateRandomGraph(10000, 7000, GraphAdjacencyList)
-         generateRandomGraph(50000, 35000, GraphAdjacencyList)
-         generateRandomGraph(100000, 70000, GraphAdjacencyList)
+
+    for k in range (1,4):
+
+         generateRandomGraph(500, 200, GraphAdjacencyList)
+         generateRandomGraph(1000, 400, GraphAdjacencyList)
+         generateRandomGraph(2000, 800, GraphAdjacencyList)
+         generateRandomGraph(5000, 1000, GraphAdjacencyList)
+         generateRandomGraph(10000, 2000, GraphAdjacencyList)
+         generateRandomGraph(20000, 4000, GraphAdjacencyList)
+         generateRandomGraph(50000, 10000, GraphAdjacencyList)
+         generateRandomGraph(100000, 20000, GraphAdjacencyList)
+         generateRandomGraph(200000, 40000, GraphAdjacencyList)
+         generateRandomGraph(500000, 100000, GraphAdjacencyList)
+
+         generateRandomGraph(500, 400, GraphAdjacencyList)
+         generateRandomGraph(1000, 800, GraphAdjacencyList)
+         generateRandomGraph(2000, 1600, GraphAdjacencyList)
+         generateRandomGraph(5000, 4000, GraphAdjacencyList)
+         generateRandomGraph(10000, 8000, GraphAdjacencyList)
+         generateRandomGraph(20000, 16000, GraphAdjacencyList)
+         generateRandomGraph(50000, 40000, GraphAdjacencyList)
+         generateRandomGraph(100000, 80000, GraphAdjacencyList)
+         generateRandomGraph(200000, 160000, GraphAdjacencyList)
+         generateRandomGraph(500000, 400000, GraphAdjacencyList)
+
+
+
